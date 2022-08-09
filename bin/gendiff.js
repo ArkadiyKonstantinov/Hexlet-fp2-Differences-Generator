406359cfa4de3ab9f47baf9a6d6c5284b18a64b8
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-08-09 15:06:51 +0300

    Complete step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,9 @@ import { Command } from 'commander';
 const gendiff = new Command();
 gendiff
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-gendiff.parse();
+  .version('1.0.0')
+  .usage('[options] <filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
+gendiff.parse(process.argv);
