1eed7e395a1249f5411c77cebcdd064196dbd60c
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-09-06 16:47:42 +0300

    Feat: plane formatter

--- src/index.js
+++ src/index.js
@@ -116,12 +116,62 @@ const stylish = (differences, replacer = ' ', spacesCount = 2) => {
   return iter(differences, 1);
 };
 
+const getCurrentValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const plane = (differences, keyPath = []) => {
+  const keys = Object.keys(differences).sort();
+  const lines = keys
+    .flatMap((key) => {
+      const currentKeyPath = [...keyPath, key];
+      const { type, value } = differences[key];
+      let result;
+      switch (type) {
+        case 'nested': {
+          result = plane(value, currentKeyPath);
+          break;
+        }
+        case 'added': {
+          const currentValue = getCurrentValue(value);
+          result = `Property '${currentKeyPath.join('.')}' was ${type} with value: ${currentValue}`;
+          break;
+        }
+        case 'removed': {
+          result = `Property '${currentKeyPath.join('.')}' was ${type}`;
+          break;
+        }
+        case 'updated': {
+          const { old, updated } = value;
+          const currentOld = getCurrentValue(old);
+          const currentUpdated = getCurrentValue(updated);
+          result = `Property '${currentKeyPath.join('.')}' was ${type}. From ${currentOld} to ${currentUpdated}`;
+          break;
+        }
+        default: {
+          result = [];
+          break;
+        }
+      }
+      return result;
+    });
+  return lines.join('\n');
+};
+
 export default (path1, path2, format = 'stylish') => {
   const obj1 = parseFile(readFile(path1));
   const obj2 = parseFile(readFile(path2));
   let formater;
   if (format === 'stylish') {
     formater = stylish;
+  } else if (format === 'plane') {
+    formater = plane;
   }
   return formater(genDifferencesTree(obj1, obj2));
 };
