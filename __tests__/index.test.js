256638349d38d07cdf3cf84cf9147fd3ef13a4fa
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-09-06 11:19:34 +0300

    Add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,33 +7,34 @@ import genDiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const casesPlane = [
-  ['plane1.json', 'plane2.json'],
-  ['plane1.yml', 'plane2.yml'],
-  ['plane1.yaml', 'plane2.yaml'],
-  ['plane1.yml', 'plane2.yaml'],
-  ['plane1.json', 'plane2.yaml'],
+const expectedData = fs.readFileSync(getFixturePath('result.txt'), 'utf-8').split('\n\n\n');
+
+const casesSimple = [
+  ['simple1.json', 'simple2.json', undefined, expectedData[0]],
+  ['simple1.yml', 'simple2.yml', undefined, expectedData[0]],
+  ['simple1.yaml', 'simple2.yaml', 'stylish', expectedData[0]],
+  ['simple1.yml', 'simple2.yaml', 'stylish', expectedData[0]],
+  ['simple1.yml', 'simple2.yaml', 'plane', expectedData[1]],
+  ['simple1.json', 'simple2.yaml', 'plane', expectedData[1]],
 ];
 
 const casesNested = [
-  ['nested1.json', 'nested2.json'],
-  ['nested1.yml', 'nested2.yml'],
-  ['nested1.yaml', 'nested2.yaml'],
-  ['nested1.yml', 'nested2.yaml'],
-  ['nested1.json', 'nested2.yaml'],
+  ['nested1.json', 'nested2.json', undefined, expectedData[2]],
+  ['nested1.yml', 'nested2.yml', undefined, expectedData[2]],
+  ['nested1.yaml', 'nested2.yaml', 'stylish', expectedData[2]],
+  ['nested1.json', 'nested2.yaml', 'stylish', expectedData[2]],
+  ['nested1.yml', 'nested2.json', 'plane', expectedData[3]],
+  ['nested1.yaml', 'nested2.yml', 'plane', expectedData[3]],
 ];
 
-const expectedPlain = fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8');
-const expectedNested = fs.readFileSync(getFixturePath('resultNested.txt'), 'utf-8');
-
-describe.each(casesPlane)('plane', (file1, file2) => {
+describe.each(casesSimple)('genDiff simple files', (file1, file2, format, expected) => {
   const path1 = getFixturePath(file1);
   const path2 = getFixturePath(file2);
-  test('plain object', () => expect(genDiff(path1, path2)).toEqual(expectedPlain));
+  test(`File ${file1} and file ${file2} with formater ${format}`, () => expect(genDiff(path1, path2, format)).toEqual(expected));
 });
 
-describe.each(casesNested)('nested', (file1, file2) => {
+describe.each(casesNested)('genDiff nested files', (file1, file2, format, expected) => {
   const path1 = getFixturePath(file1);
   const path2 = getFixturePath(file2);
-  test('nested object', () => expect(genDiff(path1, path2)).toEqual(expectedNested));
+  test(`File ${file1} and file ${file2} with formater ${format}`, () => expect(genDiff(path1, path2, format)).toEqual(expected));
 });
