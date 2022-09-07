1bc26757d75b3856da703080df286ad8ba84a708
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-09-07 22:37:56 +0300

    Refactoring plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,29 +10,32 @@ const stringify = (value) => {
   return `${value}`;
 };
 
-const plain = (differences, path = []) => {
-  const lines = differences
-    .flatMap((diff) => {
-      const currentPath = [...path, diff.key];
-      switch (diff.type) {
-        case 'nested': {
-          return plain(diff.children, currentPath);
+const plain = (differences) => {
+  const iter = (currentDifferences, path = []) => {
+    const lines = currentDifferences
+      .flatMap((diff) => {
+        const currentPath = [...path, diff.key];
+        switch (diff.type) {
+          case 'nested': {
+            return iter(diff.children, currentPath);
+          }
+          case 'added': {
+            return `Property '${currentPath.join('.')}' was ${diff.type} with value: ${stringify(diff.value)}`;
+          }
+          case 'removed': {
+            return `Property '${currentPath.join('.')}' was ${diff.type}`;
+          }
+          case 'updated': {
+            return `Property '${currentPath.join('.')}' was ${diff.type}. From ${stringify(diff.old)} to ${stringify(diff.updated)}`;
+          }
+          default: {
+            return [];
+          }
         }
-        case 'added': {
-          return `Property '${currentPath.join('.')}' was ${diff.type} with value: ${stringify(diff.value)}`;
-        }
-        case 'removed': {
-          return `Property '${currentPath.join('.')}' was ${diff.type}`;
-        }
-        case 'updated': {
-          return `Property '${currentPath.join('.')}' was ${diff.type}. From ${stringify(diff.old)} to ${stringify(diff.updated)}`;
-        }
-        default: {
-          return [];
-        }
-      }
-    });
-  return lines.join('\n');
+      });
+    return lines.join('\n');
+  };
+  return iter(differences);
 };
 
 export default plain;
