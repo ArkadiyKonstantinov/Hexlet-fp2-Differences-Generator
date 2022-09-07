066f149a5706d20758ea49f1d05c0416f9436b8b
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-09-07 22:04:21 +0300

    Refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,28 +7,29 @@ import genDiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedData = fs.readFileSync(getFixturePath('result.txt'), 'utf-8').split('\n\n\n');
+const expectedSimple = fs.readFileSync(getFixturePath('resultSimple.txt'), 'utf-8').split('\n\n\n');
+const expectedNested = fs.readFileSync(getFixturePath('resultNested.txt'), 'utf-8').split('\n\n\n');
 
 const casesSimple = [
-  ['simple1.json', 'simple2.json', undefined, expectedData[0]],
-  ['simple1.yml', 'simple2.yml', undefined, expectedData[0]],
-  ['simple1.yaml', 'simple2.yaml', 'stylish', expectedData[0]],
-  ['simple1.yml', 'simple2.yaml', 'stylish', expectedData[0]],
-  ['simple1.yml', 'simple2.yaml', 'plain', expectedData[1]],
-  ['simple1.json', 'simple2.yaml', 'plain', expectedData[1]],
-  ['simple1.json', 'simple2.yaml', 'json', expectedData[2]],
-  ['simple1.json', 'simple2.yml', 'json', expectedData[2]],
+  ['simple1.json', 'simple2.json', undefined, expectedSimple[0]],
+  ['simple1.yml', 'simple2.yml', undefined, expectedSimple[0]],
+  ['simple1.yaml', 'simple2.yaml', 'stylish', expectedSimple[0]],
+  ['simple1.yml', 'simple2.yaml', 'stylish', expectedSimple[0]],
+  ['simple1.yml', 'simple2.yaml', 'plain', expectedSimple[1]],
+  ['simple1.json', 'simple2.yaml', 'plain', expectedSimple[1]],
+  ['simple1.json', 'simple2.yaml', 'json', expectedSimple[2]],
+  ['simple1.json', 'simple2.yml', 'json', expectedSimple[2]],
 ];
 
 const casesNested = [
-  ['nested1.json', 'nested2.json', undefined, expectedData[3]],
-  ['nested1.yml', 'nested2.yml', undefined, expectedData[3]],
-  ['nested1.yaml', 'nested2.yaml', 'stylish', expectedData[3]],
-  ['nested1.json', 'nested2.yaml', 'stylish', expectedData[3]],
-  ['nested1.yml', 'nested2.json', 'plain', expectedData[4]],
-  ['nested1.yaml', 'nested2.yml', 'plain', expectedData[4]],
-  ['nested1.json', 'nested2.yml', 'json', expectedData[5]],
-  ['nested1.yaml', 'nested2.yml', 'json', expectedData[5]],
+  ['nested1.json', 'nested2.json', undefined, expectedNested[0]],
+  ['nested1.yml', 'nested2.yml', undefined, expectedNested[0]],
+  ['nested1.yaml', 'nested2.yaml', 'stylish', expectedNested[0]],
+  ['nested1.json', 'nested2.yaml', 'stylish', expectedNested[0]],
+  ['nested1.yml', 'nested2.json', 'plain', expectedNested[1]],
+  ['nested1.yaml', 'nested2.yml', 'plain', expectedNested[1]],
+  ['nested1.json', 'nested2.yml', 'json', expectedNested[2]],
+  ['nested1.yaml', 'nested2.yml', 'json', expectedNested[2]],
 ];
 
 describe.each(casesSimple)('genDiff simple files', (file1, file2, format, expected) => {
