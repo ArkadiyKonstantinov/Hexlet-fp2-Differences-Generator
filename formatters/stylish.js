bb94040df48554b8f6d4cc8d4915a5d88d6e2fd5
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-09-07 18:50:54 +0300

    Refactoring formatter stylish

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,66 +1,53 @@
 import _ from 'lodash';
 
-const replacer = ' ';
-const spacesCount = 2;
+const getIndent = (position, level) => {
+  const replacer = ' ';
+  const spacesCount = 2;
+  const prefixSize = spacesCount * level;
+  const indent = {
+    current: `${replacer.repeat(prefixSize)}`,
+    bracket: `${replacer.repeat(prefixSize - spacesCount)}`,
+  };
+  return indent[position];
+};
 
 const stringify = (currentValue, level = 1) => {
   if (!_.isObject(currentValue)) {
     return `${currentValue}`;
   }
-  const prefixSize = spacesCount * level;
-  const start = '{';
-  const currentIndent = `${replacer.repeat(prefixSize)}`;
-  const end = `${replacer.repeat(prefixSize - spacesCount)}}`;
   const keys = Object.keys(currentValue);
   const strings = keys
-    .map((key) => `${currentIndent}${key}: ${stringify(currentValue[key], level + 1)}`);
-  return [start, ...strings, end].join('\n');
+    .map((key) => `${getIndent('current', level)}${key}: ${stringify(currentValue[key], level + 1)}`);
+  return ['{', ...strings, `${getIndent('bracket', level)}}`].join('\n');
 };
 
 const stylish = (differences) => {
   const iter = (currentDiff, level = 1) => {
     const keys = Object.keys(currentDiff).sort();
-    const prefixSize = spacesCount * level;
-    const start = '{';
-    const currentIndent = `${replacer.repeat(prefixSize)}`;
-    const end = `${replacer.repeat(prefixSize - spacesCount)}}`;
     const lines = keys
       .flatMap((key) => {
         const { type, value } = currentDiff[key];
-        let result = [];
         switch (type) {
           case 'nested': {
-            const prefix = '  ';
-            result = [`${currentIndent}${prefix}${key}: ${iter(value, level + 1)}`];
-            break;
+            return [`${getIndent('current', level)}  ${key}: ${iter(value, level + 1)}`];
           }
           case 'added': {
-            const prefix = '+ ';
-            result = [`${currentIndent}${prefix}${key}: ${stringify(value, level + 1)}`];
-            break;
+            return [`${getIndent('current', level)}+ ${key}: ${stringify(value, level + 1)}`];
           }
           case 'removed': {
-            const prefix = '- ';
-            result = [`${currentIndent}${prefix}${key}: ${stringify(value, level + 1)}`];
-            break;
+            return [`${getIndent('current', level)}- ${key}: ${stringify(value, level + 1)}`];
           }
           case 'updated': {
             const { old, updated } = value;
-            const prefixOld = '- ';
-            const prifixUpdatet = '+ ';
-            result = [`${currentIndent}${prefixOld}${key}: ${stringify(old, level + 1)}`,
-              `${currentIndent}${prifixUpdatet}${key}: ${stringify(updated, level + 1)}`];
-            break;
+            return [`${getIndent('current', level)}- ${key}: ${stringify(old, level + 1)}`,
+              `${getIndent('current', level)}+ ${key}: ${stringify(updated, level + 1)}`];
           }
-          case 'equal': {
-            const prefix = '  ';
-            result = [`${currentIndent}${prefix}${key}: ${stringify(value, level + 1)}`];
-            break;
+          default: { // case 'equal'
+            return [`${getIndent('current', level)}  ${key}: ${stringify(value, level + 1)}`];
           }
         }
-        return result;
       });
-    return [start, ...lines, end].join('\n');
+    return ['{', ...lines, `${getIndent('bracket', level)}}`].join('\n');
   };
   return iter(differences, 1);
 };
