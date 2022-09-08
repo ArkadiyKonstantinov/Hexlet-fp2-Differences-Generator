84bf4ca3545db761c011a7351e610566f1daa366
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2022-09-08 14:42:12 +0300

    Review 1

---
+++ src/differences.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+const genDifferences = (left, right) => {
+  const keys = _.sortBy(_.union(Object.keys(left), Object.keys(right)));
+  return keys
+    .map((key) => {
+      const leftValue = _.cloneDeep(left[key]);
+      const rightValue = _.cloneDeep(right[key]);
+      if (_.isObject(left[key]) && _.isObject(right[key])) {
+        return { key, type: 'nested', children: genDifferences(left[key], right[key]) };
+      }
+      if (!Object.hasOwn(left, key)) {
+        return { key, type: 'added', value: rightValue };
+      }
+      if (!Object.hasOwn(right, key)) {
+        return { key, type: 'removed', value: leftValue };
+      }
+      if (!Object.is(left[key], right[key])) {
+        return {
+          key, type: 'updated', old: leftValue, updated: rightValue,
+        };
+      }
+      return { key, type: 'equal', value: leftValue };
+    });
+};
+
+export default genDifferences;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,19 @@
-import _ from 'lodash';
-import path from 'path';
-import fs from 'fs';
+import { resolve, extname } from 'path';
+import { readFileSync } from 'fs';
+import genDifferences from './differences.js';
 import parse from './parsers.js';
 import format from './formatters/index.js';
 
 const parseFile = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath);
-  const extName = path.extname(fullPath).slice(1);
-  const data = fs.readFileSync(fullPath);
+  const fullPath = resolve(process.cwd(), filepath);
+  const extName = extname(fullPath).slice(1);
+  const data = readFileSync(fullPath);
   return parse(data, extName);
 };
 
-const genDifferences = (left, right) => {
-  const keys = _.sortBy(_.union(Object.keys(left), Object.keys(right)));
-  return keys
-    .map((key) => {
-      const leftValue = _.cloneDeep(left[key]);
-      const rightValue = _.cloneDeep(right[key]);
-      if (_.isObject(left[key]) && _.isObject(right[key])) {
-        return { key, type: 'nested', children: genDifferences(left[key], right[key]) };
-      }
-      if (!Object.hasOwn(left, key)) {
-        return { key, type: 'added', value: rightValue };
-      }
-      if (!Object.hasOwn(right, key)) {
-        return { key, type: 'removed', value: leftValue };
-      }
-      if (!Object.is(left[key], right[key])) {
-        return {
-          key, type: 'updated', old: leftValue, updated: rightValue,
-        };
-      }
-      return { key, type: 'equal', value: leftValue };
-    });
-};
-
 export default (path1, path2, formatName = 'stylish') => {
-  const obj1 = parseFile(path1);
-  const obj2 = parseFile(path2);
-  const diff = genDifferences(obj1, obj2);
+  const parsedFile1 = parseFile(path1);
+  const parsedFile2 = parseFile(path2);
+  const diff = genDifferences(parsedFile1, parsedFile2);
   return format(diff, formatName);
 };
